e6d05a5a605aabca51e8f76a26ce8471b1333e8b
Peter Marsh <peter_marsh@hotmail.com>
2020-02-10 17:33:38 -0800

    Circles Rotate

--- src/Drawing.java
+++ src/Drawing.java
@@ -1,67 +1,84 @@
 
 import java.awt.Canvas;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.ArrayList;
+import java.util.Timer;
 
 import javax.swing.JFrame;
 
 
-public class Drawing extends Canvas implements KeyListener{
+public class Drawing extends Canvas {
 
     
-	public boolean drawing = true;
-	
-	public void keyPressed(KeyEvent e) {
-
-	    int key = e.getKeyCode();
-
-	    if(key == KeyEvent.KEY_PRESSED){
-
-	        drawing = false;
-	        System.out.println("CUTTTTT");
-	    }
-	}
+	public static boolean drawing = true;
+	public static ArrayList<Rectangle> rectangles;
+	public static Timer t;
+	public static int degree;
 
 	
 	
 	public void paint(Graphics g) {
 		
-		ArrayList<Rectangle> rectangles = new ArrayList<>();
+		rectangles = new ArrayList<>();
 		
 		double sideLength = 5;
     	for (int i = 0; i < 30; i++) {
-    		rectangles.add(centerRect(200,200,(int)sideLength));
+    		rectangles.add(centerRect(200,200, (int)sideLength, (int)sideLength));
     		sideLength += 10;
     	}
-		
-		//while (drawing) {
-			
-			for (Rectangle rect: rectangles) {
-				drawOvalFrom(g, rect);
-				
-			}
-			
-			
-			
-			
-			
-			
-			
-			
-			
-		//}
+    	
+		animationLoop(g);
 		
     }
+	
+	public static void update() {
+		degree += 5;
+		System.out.println("degree: " + degree);
+	}
+	
+	public static void render(Graphics g) {
+		//refresh background
+		g.setColor(Color.white);
+		g.fillRect(0, 0, 500, 500);
+		//draw circles
+		g.setColor(Color.black);
+		for (Rectangle rect: rectangles) {
+				drawOvalFrom(g, rectangleAtDegree(rect, degree));
+		}	
+		
+	}
+	
+	public static Rectangle rectangleAtDegree(Rectangle r, int degree) {
+		int centerX = r.x + r.width/2;
+		double width = r.width * Math.abs(Math.cos((degree/360.0)*2*Math.PI));
+		return centerRect(centerX, 200, (int)width, r.height);
+	}
+	
+	
+	public static void animationLoop(Graphics g) 
+    	{
+    	    while(drawing) //the loop
+    	    {
+    	        update();
+    	        render(g);
+    	        try {
+    	            Thread.sleep(30);                 
+    	        } catch(InterruptedException ex) {
+    	            Thread.currentThread().interrupt();
+    	        }
+    	    }
+    	}
     
-	public void drawOvalFrom(Graphics g, Rectangle rect) {
+	public static void drawOvalFrom(Graphics g, Rectangle rect) {
 		g.drawOval(rect.x, rect.y, rect.width, rect.height);
 	}
 	
-    public Rectangle centerRect(int centerX, int centerY, int sideLength) {
-    	return new Rectangle(centerX - (sideLength/2), centerY - (sideLength/2), sideLength, sideLength);
+    public static Rectangle centerRect(int centerX, int centerY, int width, int height) {
+    	return new Rectangle(centerX - (width/2), centerY - (height/2), width, height);
     }
 	
 	
@@ -72,23 +89,18 @@ public class Drawing extends Canvas implements KeyListener{
         frame.add(canvas);
         frame.pack();
         frame.setVisible(true);
+        Listener listener = new Listener();
+        frame.addKeyListener(listener);
+        
+        
         
         
     }
 
 
 
-	@Override
-	public void keyTyped(KeyEvent e) {
-		// TODO Auto-generated method stub
-		
-	}
-
-
-
-	@Override
-	public void keyReleased(KeyEvent e) {
-		// TODO Auto-generated method stub
+	public static void stopDrawing() {
+		drawing = false;
 		
 	}
 
